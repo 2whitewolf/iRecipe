fb36337e7128752c3aad2962c83b5cad69b76763
Bogdan Sevcenco <bogdan@MacBook-Pro-Bogdan.local>
2023-04-18 17:50:13 +0300

    initilize project

---
+++ iRecipe/iRecipe/iRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iRecipeApp.swift
+//  iRecipe
+//
+//  Created by Bogdan Sevcenco on 18.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct iRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
